533da92f2812fd84309bf5dd97f3e592e416dd8b
Булай Вадим Андреевич <a.bulay@rit.va>
2023-09-22 18:58:04 +0300

    create logic to task-1

--- R4C/settings.py
+++ R4C/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mztx@x_-=gfhc9xs@bm58m&@3pc7##opo14zob!(l2tus05+jo'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
---
+++ robots/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from robots.views import create_robot
+
+urlpatterns = [
+    path('create/', create_robot, name='robot-create'),
+]
