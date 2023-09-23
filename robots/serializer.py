f51d5a153f1c965dc760be332f7d8ffa135b8b7f
Булай Вадим Андреевич <a.bulay@rit.va>
2023-09-23 12:25:54 +0300

    add created validation

--- robots/serializer.py
+++ robots/serializer.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from robots.models import Robot
-import re
+from datetime import datetime
 
 
 class RobotSerializer:
@@ -12,9 +12,16 @@ class RobotSerializer:
         try:
             model = self.data['model']
             version = self.data['version']
+            created = self.data['created']
+
+            try:
+                datetime.strptime(created, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                raise ValidationError('Invalid datetime format. Must be in the format "YYYY-MM-DD HH:MM:SS"')
 
             if model not in ['R2', '13', 'X5']:
                 raise ValidationError('Invalid robot model. Must be in the format "R2", "13", "X5"')
+
             if version not in ['D2', 'XS', 'LT']:
                 raise ValidationError('Invalid robot version. Must be in the format "D2", "XS", "LT"')
 
